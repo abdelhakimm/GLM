fe721097bbd196610a62ecc47112d5ee45e17f09
unknown <benjamin.martel@live.fr>
2023-05-02 21:49:11 +0200

    entity creer contract

---
+++ src/Entity/Contract.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContractRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContractRepository::class)]
+class Contract
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $start_date_contract = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $end_date_contract = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $trype_contract = null;
+
+    #[ORM\Column]
+    private ?int $hour_contract = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStartDateContract(): ?\DateTimeInterface
+    {
+        return $this->start_date_contract;
+    }
+
+    public function setStartDateContract(\DateTimeInterface $start_date_contract): self
+    {
+        $this->start_date_contract = $start_date_contract;
+
+        return $this;
+    }
+
+    public function getEndDateContract(): ?\DateTimeInterface
+    {
+        return $this->end_date_contract;
+    }
+
+    public function setEndDateContract(\DateTimeInterface $end_date_contract): self
+    {
+        $this->end_date_contract = $end_date_contract;
+
+        return $this;
+    }
+
+    public function getTrypeContract(): ?string
+    {
+        return $this->trype_contract;
+    }
+
+    public function setTrypeContract(string $trype_contract): self
+    {
+        $this->trype_contract = $trype_contract;
+
+        return $this;
+    }
+
+    public function getHourContract(): ?int
+    {
+        return $this->hour_contract;
+    }
+
+    public function setHourContract(int $hour_contract): self
+    {
+        $this->hour_contract = $hour_contract;
+
+        return $this;
+    }
+}
