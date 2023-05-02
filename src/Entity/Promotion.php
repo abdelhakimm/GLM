2f6a58a29111d435966e9b2e59e76a6603cebffb
unknown <benjamin.martel@live.fr>
2023-05-02 22:03:49 +0200

    entity Promotion

---
+++ src/Entity/Promotion.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PromotionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PromotionRepository::class)]
+class Promotion
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $old_job = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $new_job = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOldJob(): ?string
+    {
+        return $this->old_job;
+    }
+
+    public function setOldJob(string $old_job): self
+    {
+        $this->old_job = $old_job;
+
+        return $this;
+    }
+
+    public function getNewJob(): ?string
+    {
+        return $this->new_job;
+    }
+
+    public function setNewJob(string $new_job): self
+    {
+        $this->new_job = $new_job;
+
+        return $this;
+    }
+}
