a37570a5e9e548d035940a888eb27bf01974225e
Thib Dev <thibaultdevinstep@gmail.com>
2023-05-02 20:16:18 +0200

    add Meetings Entity

---
+++ src/Entity/Meetings.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MeetingsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MeetingsRepository::class)]
+class Meetings
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_time = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $place = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $attendees = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDateTime(): ?\DateTimeInterface
+    {
+        return $this->date_time;
+    }
+
+    public function setDateTime(\DateTimeInterface $date_time): self
+    {
+        $this->date_time = $date_time;
+
+        return $this;
+    }
+
+    public function getPlace(): ?string
+    {
+        return $this->place;
+    }
+
+    public function setPlace(string $place): self
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAttendees(): ?int
+    {
+        return $this->attendees;
+    }
+
+    public function setAttendees(int $attendees): self
+    {
+        $this->attendees = $attendees;
+
+        return $this;
+    }
+}
